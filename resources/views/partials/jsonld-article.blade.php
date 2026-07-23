5a4e76450caa42ff97b4f310e204155191608ba6
Quyen <markprovjp@gmail.com>
2026-07-23 15:24:47 +0700

    fix: json-ld them JSON_HEX_TAG chong thoat script tag

--- resources/views/partials/jsonld-article.blade.php
+++ resources/views/partials/jsonld-article.blade.php
@@ -30,4 +30,4 @@
         return !is_null($value) && $value !== '';
     });
 @endphp
-<script type="application/ld+json">@json($schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)</script>
+<script type="application/ld+json">@json($schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT)</script>
--- resources/views/partials/jsonld-person.blade.php
+++ resources/views/partials/jsonld-person.blade.php
@@ -20,4 +20,4 @@
         return !is_null($value) && $value !== '';
     });
 @endphp
-<script type="application/ld+json">@json($schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)</script>
+<script type="application/ld+json">@json($schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT)</script>
