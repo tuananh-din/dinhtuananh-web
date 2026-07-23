7ed7fa9209aad4ae33af7fb6321fba6d9d0c7141
Quyen <markprovjp@gmail.com>
2026-07-23 15:53:25 +0700

    feat: json-ld person them sameAs tu social links

--- resources/views/partials/jsonld-person.blade.php
+++ resources/views/partials/jsonld-person.blade.php
@@ -14,9 +14,19 @@
         'image' => $avatar,
         'jobTitle' => data_get($person, 'description'),
         'description' => data_get($person, 'about_me') ?: data_get($person, 'content'),
+        'sameAs' => array_values(array_filter([
+            data_get($person, 'facebook'),
+            data_get($person, 'instagram'),
+            data_get($person, 'linkedin'),
+            data_get($person, 'x'),
+        ])),
     ];
 
     $schema = array_filter($schema, function ($value) {
+        if (is_array($value)) {
+            return !empty($value);
+        }
+
         return !is_null($value) && $value !== '';
     });
 @endphp
